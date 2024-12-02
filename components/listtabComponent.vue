f32e69c4de8194b8427c05bc357bc4e485054e65
omerbozkurts <omerbozkurt912@gmail.com>
2024-12-02 07:44:56 +0300

    list tab update

--- components/listtabComponent.vue
+++ components/listtabComponent.vue
@@ -10,10 +10,37 @@
                   >
                   {{ tab.name }}
               </button>
+          </div>   
+          <div class="sekme-icerik">
+            <div v-if="currentTab === 0" class="sekme-urun">
+              <AlisverisKartComponent resim="/urunler/scrikss-proxi-1-2-mm-yesil-tukenmez-kalem-22_min.webp" 
+                    urun-ismi="Scrikss Proxi 1.2 mm Yeşil Tükenmez Kalem" ilk-fiyat="45.00" indirim-orani="30" indirimli-fiyat="31.50"
+                ></AlisverisKartComponent>
+                <AlisverisKartComponent resim="/urunler/writech-w-0171-pembe-mavi-govde-0-7-mm-mavi-jel-kalem-11_min.webp" 
+                    urun-ismi="Writech W-0171 Mor Mavi Gövde 0.7 mm Mavi Jel Kalem" ilk-fiyat="64.00" indirim-orani="15" indirimli-fiyat="54.40"
+                ></AlisverisKartComponent>
+                <AlisverisKartComponent resim="/urunler/pelikan-490-pastel-pembe-fosforlu-kalem-111_min.webp" 
+                    urun-ismi="Pelikan 490 Pastel Pembe Fosforlu Kalem" ilk-fiyat="50.00" indirim-orani="25" indirimli-fiyat="37.50"
+                ></AlisverisKartComponent>
+                <AlisverisKartComponent resim="/urunler/pin-12-mmx33-m-ofis-bandi-11_min.webp" 
+                    urun-ismi="Pin 12 mm*33 m Ofis Bandı" ilk-fiyat="14.00" indirim-orani="35" indirimli-fiyat="9.10"
+                ></AlisverisKartComponent>
+                <AlisverisKartComponent resim="/urunler/vinson-fashion-a26-papatya-0-7-mm-mavi-jel-kalem-11_min.webp" 
+                    urun-ismi="Vinson Fashion A26 Papatya 0.7 mm Mavi Jel Kalem" ilk-fiyat="52.00" indirim-orani="25" indirimli-fiyat="39.00"
+                ></AlisverisKartComponent>
+                <AlisverisKartComponent resim="/urunler/rubenis-okul-etiket-1_min.webp" 
+                    urun-ismi="Rubenis 3'lü Kırmızı Okul Etiketi" ilk-fiyat="25.00" indirim-orani="40" indirimli-fiyat="15.00"
+                ></AlisverisKartComponent>
+                <AlisverisKartComponent resim="/urunler/pelikan-490-pastel-mor-fosforlu-kalem-11_min.webp" 
+                    urun-ismi="Pelikan 490 Pastel Mor Fosforlu Kalem" ilk-fiyat="50.00" indirim-orani="25" indirimli-fiyat="37.50"
+                ></AlisverisKartComponent>
+                <AlisverisKartComponent resim="/urunler/kr-akademi-tyt-vip-fizik-soru-bankasi_min.webp" 
+                    urun-ismi="KR Akademi TYT Vip Fizik Soru Bankası" ilk-fiyat="249.00" indirim-orani="20" indirimli-fiyat="199.20"
+                ></AlisverisKartComponent>
+            </div>
+            <div v-if="currentTab === 1" class="sekme-urun"></div>
+            <div v-if="currentTab === 2" class="sekme-urun"></div>
           </div>
-          
-              
-          
       </div>
   </div>
 </template>
@@ -39,7 +66,7 @@
   display: flex;
   justify-content: center;
   gap: 20px; 
-  margin-bottom: 20px;
+  margin-bottom: 50px;
   align-items: center;
 }
 
@@ -57,10 +84,10 @@
 }
 
 .tab-button.active {
-  background-color: #ff6f00; 
+  background-color: #ff6000; 
   color: white;
   font-weight: bold;
-  border-color: #ff6f00;
+  border-color: #ff6000;
 }
 
 .tab-buttons::before,
@@ -73,6 +100,17 @@
   margin-left: var(--spacer-8);
 }
 
+.sekme-icerik{
+ max-width: 100%;
+ height: auto;
+}
+
+.sekme-urun{
+  display: grid;
+  grid-template-columns: repeat(4, 1fr); 
+  width: 100%; 
+  margin: 0 auto; 
+}
 
 </style>
 
